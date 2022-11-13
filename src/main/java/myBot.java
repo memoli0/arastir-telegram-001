97e3de4586d4ebb0ab22eeaa91a8698523e4d7d0
memoli0 <70433403+memoli0@users.noreply.github.com>
2022-11-13 11:00:15 +0300

    update

--- src/main/java/myBot.java
+++ src/main/java/myBot.java
@@ -1,7 +1,11 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
 
 public class myBot extends TelegramLongPollingBot {
     @Override
@@ -17,6 +21,55 @@ public class myBot extends TelegramLongPollingBot {
             response.setChatId(chat_id);
             response.setText("Hey bro");
 
+
+            try {
+                execute(response); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+
+        }else if(message_text.equals("/kaynak kategorileri")){
+
+            SendMessage response = new SendMessage();
+            response.setChatId(chat_id);
+            response.setText("Kategorileri klavyene yönlendiriyorum.");
+
+            // Create ReplyKeyboardMarkup object
+            ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+            // Create the keyboard (list of keyboard rows)
+            ArrayList<KeyboardRow> keyboard = new ArrayList<>();
+            // Create a keyboard row
+            KeyboardRow row = new KeyboardRow();
+            // Set each button, you can also use KeyboardButton objects if you need something else than text
+            row.add("arasTiR kaynak kodları");
+            row.add("Android Studio kodlarla anlatım");
+            row.add("Telegram bot yapımı");
+            // Add the first row to the keyboard
+            keyboard.add(row);
+            // Create another keyboard row
+            row = new KeyboardRow();
+            // Set each button for the second line
+            row.add("Okuma kitabı kaynakları");
+            row.add("Ders kitabı kaynakları");
+            row.add("Video ders anlatım kaynakları");
+            // Add the second row to the keyboard
+            keyboard.add(row);
+            // Set the keyboard to the markup
+            keyboardMarkup.setKeyboard(keyboard);
+            // Add it to the message
+            response.setReplyMarkup(keyboardMarkup);
+
+            try {
+                execute(response); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+
+        }else{
+
+            SendMessage response = new SendMessage();
+            response.setChatId(chat_id);
+            response.setText("Ne dediğini anlayamadım.");
 
             try {
                 execute(response); // Call method to send the message
